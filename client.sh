377357b017273a18e2461beff775761e6b948e2e
Robert Herschel Hawk <robert@the-hawk.us>
2018-04-03 16:15:18 -0700

    Updated client for use with THUS modpacks

--- client.sh
+++ client.sh
@@ -1,7 +1,29 @@
-if [ "$1" == "update" ]
+if [ "$1" == "install" ]
 then
-    cd "$2"
-    git pull
+    if [ -d "$HOME/minecraft/$2" ]
+    then
+        echo "$2 is already installed"
+    else
+        cd "$HOME/minecraft"
+        git clone "thusmc@mc.the-hawk.us:$2"
+    fi
+elif [ "$1" == "update" ]
+then
+    if [ -d "$HOME/minecraft/$2" ]
+    then
+        cd "$HOME/minecraft/$2"
+        git pull
+    else
+        echo "$2 is not an installed modpack"
+    fi
+elif [ "$1" == "version" ]
+then
+    if [ -d "$HOME/minecraft/$2" ]
+    then
+        cat "$HOME/minecraft/$2/.thusinfo"
+    else
+        echo "$2 is not an installed modpack"
+    fi
 else
     java -jar $HOME/lib/Minecraft.jar
 fi
