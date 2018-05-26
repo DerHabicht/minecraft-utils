a09b0d1138e45565c37fc5282e7bd0f55c0b8791
Robert Herschel Hawk <robert@the-hawk.us>
2018-05-26 15:31:26 -0700

    Clean repo before update
    Some mods seem to do a lot of writing to their own config files for no
    apparent reason. So, before an update, clean the working directory. This
    is a really bad way to do it, but I also don't really care right now.

--- client.sh
+++ client.sh
@@ -1,18 +1,59 @@
 if [ "$1" == "install" ]
 then
-    if [ -d "$HOME/minecraft/$2" ]
+    if [ -d "$HOME/.minecraft" ]
     then
-        echo "$2 is already installed"
+        if [ ! -d "$HOME/minecraft" ]
+        then
+            mkdir "$HOME/minecraft"
+        fi
+        if [ -d "$HOME/minecraft/$2" ]
+        then
+            echo "$2 is already installed"
+        else
+            cd "$HOME/minecraft"
+            git clone "thusmc@mc.the-hawk.us:$2"
+            if [ "$?" != 0 ]
+            then
+                echo ""
+                echo "Sorry, it looks like $2 is not a valid modpack."
+                echo "$2 could not be installed."
+                exit
+            fi
+            cd "$2"
+            source .thusinfo
+            java -jar "forge-$FORGE-installer.jar"
+        fi
     else
-        cd "$HOME/minecraft"
-        git clone "thusmc@mc.the-hawk.us:$2"
+        echo "Minecraft has not run on this system yet."
+        echo "You will need to run Minecraft before installing a modpack."
+        echo "Do you want to run the Minecraft JAR now?"
+        read choice
+
+        if [ "$choice" == "yes" ]
+        then
+            if [ ! -f "$HOME/lib/Minecraft.jar" ]
+            then
+                wget -O $HOME/lib/Minecraft.jar http://s3.amazonaws.com/Minecraft.Download/launcher/Minecraft.jar
+            fi
+            java -jar $HOME/lib/Minecraft.jar
+        fi
     fi
 elif [ "$1" == "update" ]
 then
     if [ -d "$HOME/minecraft/$2" ]
     then
         cd "$HOME/minecraft/$2"
+        source .thusinfo
+        OLDFORGE=$FORGE
+        git reset --hard HEAD
         git pull
+        source .thusinfo
+        if [ "$OLDFORGE" != "$FORGE" ]
+        then
+            echo "This pack uses a new version of Minecraft Forge."
+            echo "The Forge installer will launch now."
+            java -jar "forge-$FORGE-installer.jar"
+        fi
     else
         echo "$2 is not an installed modpack"
     fi
@@ -20,7 +61,8 @@ elif [ "$1" == "version" ]
 then
     if [ -d "$HOME/minecraft/$2" ]
     then
-        cat "$HOME/minecraft/$2/.thusinfo"
+        source "$HOME/minecraft/$2/.thusinfo"
+        echo "$VERSION"
     else
         echo "$2 is not an installed modpack"
     fi
